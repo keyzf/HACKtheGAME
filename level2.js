db596ea0ecd83b509e575d0c23c013a37e906df3
Maxime EUZIERE <maxime.euziere@gmail.com>
2013-06-28 19:57:40 +0200

    improvements

--- level2.js
+++ level2.js
@@ -1,22 +1,22 @@
 var c1 = CodeMirror.fromTextArea(document.getElementById("code1"), {readOnly: 'nocursor'});
 var c2 = CodeMirror.fromTextArea(document.getElementById("code2"));
-
 var background = document.getElementById("background").getContext("2d");
 background.fillStyle = "#afa";
-background.fillRect (230, 230, 40, 270);
-
+background.fillRect (270, 270, 40, 330);
 try{
-  eval(c1.getValue().replace("var game = {}", "var game = {}; \n game.gravity = function(){};") + c2.getValue());
-  eval(c1.getValue().replace("var game = {};", "//var game = {}") + c2.getValue());
+  eval(c1.getValue() + c2.getValue());
 }
 catch(e){
   console.log(e);
 }
-
 document.getElementById("code").onkeyup = function(){
+  if(hero.y > 600){
+    hero.y = 275;
+    gravity = function(){};
+  }
   try{
-    game.gravity = function(){}
-    eval(c2.getValue());
+    gravity = function(){}
+    eval(c1.getValue() + c2.getValue());
   }
   catch(e){
     console.log(e);
@@ -26,17 +26,13 @@ var steps = 0;
 var orig_drawImage = CanvasRenderingContext2D.prototype.drawImage;
 CanvasRenderingContext2D.prototype.drawImage = function(a,x,y,u,v){
   orig_drawImage.apply(this, arguments);
-  if(x == 235 && y > 235 && y < 500){
+  if(x == 275 && y > 275){
     steps++;
-    if(y == 495 && steps == 52){
+    if(y == 595 && steps == 127){
       alert("well done! next level!");
       window.location = "level3.html";
     }
   }
-  if(y > 500){
-    y = 235;
-    game.gravity = function(){};
-  }
 };
 
-// answer: game.gravity = function(){game.hero.y += game.gravityspeed}
+// answer: function gravity(){hero.y += g} */
